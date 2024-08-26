ccbff80987446057f66f6b273d7d9455b6f448de
mostafa dadfar <dadfarjs@gmail.com>
2024-08-26 20:35:05 +0330

    initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+
+const store = useCounterStore()
+
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button @click="store?.increment">{{ store.count }}</button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
